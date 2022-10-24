39e2469e9b273643c79715c2c70cc5ca7cac27a9
Jonny Graham <jnagrah@amazon.com>
2022-10-24 21:52:06 +0300

    Initial commit

---
+++ test/bet_knesset_times.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BetKnessetTimes from '../lib/bet_knesset_times-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bet_knesset_times-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BetKnessetTimes.BetKnessetTimesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
